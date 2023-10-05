a827fb695091aaad33d48b17f11370e3b49666a6
Gabriel Rocha <rochafrgabriel@gmail.com>
2023-10-05 01:01:49 -0300

    feat: creating types Person and NewPerson and install dependencies

---
+++ src/types.rs
@@ -0,0 +1,69 @@
+use serde::{Deserialize, Serialize};
+use time::Date;
+use uuid::Uuid;
+
+#[derive(Clone, Serialize, Deserialize)]
+#[cfg_attr(feature = "sqlx", derive(sqlx::FromRow))]
+pub struct Person {
+    pub id: Uuid,
+    #[serde(rename = "nome")]
+    pub name: String,
+    #[serde(rename = "apelido")]
+    pub nick: Nick,
+    #[serde(rename = "nascimento", with = "date_format")]
+    pub birth_date: Date,
+    pub stack: Option<Vec<String>>
+}
+
+#[derive(Clone, Deserialize)]
+pub struct NewPerson {
+    pub id: Uuid,
+    #[serde(rename = "nome")]
+    pub name: String,
+    #[serde(rename = "apelido")]
+    pub nick: Nick,
+    #[serde(rename = "nascimento", with = "date_format")]
+    pub birth_date: Date,
+    pub stack: Option<Vec<String>>
+}
+
+macro_rules! new_string_type {
+    ($type:ident, max_length = $max_length:expr, error = $error_message:expr) => {
+        #[derive(Clone, Serialize, Deserialize)]
+        #[serde(try_from = "String")]
+        #[cfg_attr(feature = "sqlx", derive(sqlx::Type))]
+        #[cfg_attr(feature = "sqlx", sqlx(transparent))]
+        pub struct $type(String);
+
+        impl $type {
+            pub fn as_str(&self) -> &str {
+                &self.0
+            }
+        }
+
+        impl TryFrom<String> for $type {
+            type Error = &'static str;
+
+            fn try_from(value: String) -> Result<Self, Self::Error> {
+                if value.len() > $max_length {
+                    Err($error_message.into())
+                } else {
+                    Ok($type(value))
+                }
+            }
+        }
+
+
+        impl From<$type> for String {
+            fn from(value: $type) -> Self {
+                value.0
+            }
+        }
+    }
+}
+
+new_string_type!(Nick, max_length = 32, error = "nick is too big");
+new_string_type!(Tech, max_length = 32, error = "tech is too big");
+new_string_type!(PersonName, max_length = 100, error = "name is too big");
+
+time::serde::format_description!(date_format, Date, "[year]-[month]-[day]");
